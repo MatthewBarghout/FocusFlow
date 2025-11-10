5193e900dff5c43ee6f15172db4c64d61ee29fcf
MatthewBarghout <mattbarg@unc.edu>
2025-11-10 14:56:03 -0500

    Initial Commit

---
+++ FocusFlow/FocusFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFlowApp.swift
+//  FocusFlow
+//
+//  Created by Matthew Barghout on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FocusFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
